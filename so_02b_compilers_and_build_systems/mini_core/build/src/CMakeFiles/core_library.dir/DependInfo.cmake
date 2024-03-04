7203cca98b3f5fa3ba6acd3bb5e74a99947ab8d4
erpaffo <paffetto@gmail.com>
2024-03-04 19:29:13 +0100

    adding files

---
+++ so_02b_compilers_and_build_systems/mini_core/build/src/CMakeFiles/core_library.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/messages/base_sensor_message.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/messages/base_sensor_message.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/messages/odometry_message.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/messages/odometry_message.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/messages/pose_message.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/messages/pose_message.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/blob.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/blob.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/deserializer.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/deserializer.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/id_context.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/id_context.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/id_placeholder.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/id_placeholder.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/identifiable.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/identifiable.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/object_data.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/object_data.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/property.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/property.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/property_container.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/property_container.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/property_container_manager.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/property_container_manager.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/property_eigen.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/property_eigen.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/property_identifiable.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/property_identifiable.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/serializable.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/serializable.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/serialization_context.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/serialization_context.cpp.o"
+  "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/src/utils/serializer.cpp" "/usr/src/laboratorio/source/so_02b_compilers_and_build_systems/exercise/exercise/mini_core/build/src/CMakeFiles/core_library.dir/utils/serializer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "core_library_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
